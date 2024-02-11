a50a85fc7dd68e0fc466eda9de881e51d67f3c10
PetyaIvovaYakimova <petya_yakimova@abv.bg>
2024-02-11 10:28:52 +0200

    Update Login and Logout functionality

--- TaskBoardApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TaskBoardApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -43,12 +43,6 @@ namespace TaskBoardApp.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
             /// <summary>
@@ -84,13 +78,11 @@ namespace TaskBoardApp.Areas.Identity.Pages.Account
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
